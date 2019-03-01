f8c87a317308d5f7464320d640305b13c1d5fa4b
JC <>
2019-02-28 19:57:23 -0600

    Added combined waves test

---
+++ test.py
@@ -0,0 +1,56 @@
+import sound
+import pyaudio
+import traceback
+
+# TODO: Add test_NCombined() ?
+
+def test_TwoCombined(wave1, wave2):
+    wave3 = wave1 + wave2
+
+    stream = p.open(format=pyaudio.paFloat32,
+                    channels=1,
+                    rate=samplingRate,
+                    output=True)
+
+    stream.write(volume * wave1)
+    stream.write(volume * wave2)
+    stream.write(volume * wave3) # combined waves
+
+    stream.stop_stream()
+    stream.close()
+    
+
+def test_ThreeCombined(wave1, wave2, wave3):
+    wave4 = wave1 + wave2 + wave3
+
+    stream = p.open(format=pyaudio.paFloat32,
+                    channels=1,
+                    rate=samplingRate,
+                    output=True)
+
+    stream.write(volume * wave1)
+    stream.write(volume * wave2)
+    stream.write(volume * wave3)
+    stream.write(volume * wave4) # combined waves
+
+    stream.stop_stream()
+    stream.close()
+
+try:
+    p = pyaudio.PyAudio()
+
+    volume = 1.0
+    duration = 2.0
+    samplingRate = 44100
+
+    wave1 = sound.createWave(samplingRate, duration, 900)
+    wave2 = sound.createWave(samplingRate, duration, 440)
+    wave3 = sound.createWave(samplingRate, duration, 600)
+
+    test_TwoCombined(wave1, wave2)
+    test_ThreeCombined(wave1, wave2, wave3)
+
+except:
+    print(traceback.format_exc())
+finally:
+    p.terminate()
