ac1f0901d0831d9d2dde0f0c6e4f5ace12ae2ed8
JC <>
2019-02-28 22:13:52 -0600

    Added amplitude tuning

--- sound.py
+++ sound.py
@@ -1,8 +1,8 @@
 import numpy as np
 
-def createWave(samplingRate, duration, frequency):
+def createWave(samplingRate, duration, frequency, amplitude=1.0):
     samples = np.arange(samplingRate * duration) / samplingRate
     sinwave = 2 * np.pi * samples * frequency
-    wave = (np.sin(sinwave)).astype(np.float32)
+    wave = (amplitude * np.sin(sinwave)).astype(np.float32)
 
     return wave
--- test.py
+++ test.py
@@ -36,6 +36,18 @@ def test_ThreeCombined(wave1, wave2, wave3):
     stream.stop_stream()
     stream.close()
 
+def test_AmplitudeChange(original, changed):
+    stream = p.open(format=pyaudio.paFloat32,
+                    channels=1,
+                    rate=samplingRate,
+                    output=True)
+
+    stream.write(volume * original)
+    stream.write(volume * changed)
+
+    stream.stop_stream()
+    stream.close()
+
 try:
     p = pyaudio.PyAudio()
 
@@ -46,9 +58,11 @@ try:
     wave1 = sound.createWave(samplingRate, duration, 900)
     wave2 = sound.createWave(samplingRate, duration, 440)
     wave3 = sound.createWave(samplingRate, duration, 600)
+    wave3_1 = sound.createWave(samplingRate, duration, 600, 4)
 
     test_TwoCombined(wave1, wave2)
     test_ThreeCombined(wave1, wave2, wave3)
+    test_AmplitudeChange(wave3, wave3_1)
 
 except:
     print(traceback.format_exc())
