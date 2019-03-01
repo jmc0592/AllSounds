4c65e2af351599244b8508d045428c7280b51bf9
JC <>
2019-03-01 17:27:49 -0600

    Added x and y separation to wavemaker

--- wavemaker.py
+++ wavemaker.py
@@ -10,11 +10,23 @@ class WaveMaker():
 	def __init__(self, samplingRate):
 		self.samplingRate = samplingRate
 
-	def createSineWave(self, frequency, duration=2.0):
-		return (np.sin(2 * np.pi * np.arange(self.samplingRate * duration) * frequency/self.samplingRate)).astype(np.float32)
+	def createSineWave(self, frequency, duration=2.0, returnXCoords=False, amplitude=1.0):
+		x = self.createXCoordinates(self.samplingRate, duration)
+		if returnXCoords:
+			return self.createSineYCoordinates(self.samplingRate, x, frequency, amplitude=amplitude), x
+		return self.createSineYCoordinates(self.samplingRate, x, frequency, amplitude=amplitude)
 
 	def createSquareWave(self, frequency, duration=2.0):
 		return (sg.square(2 * np.pi * np.arange(self.samplingRate * duration) * frequency/self.samplingRate)).astype(np.float32)
 
 	def createSawtoothWave(self, frequency, duration=2.0):
 		return (sg.sawtooth(2 * np.pi * np.arange(self.samplingRate * duration) * frequency/self.samplingRate)).astype(np.float32)
+
+	def createXCoordinates(self, samplingRate, duration):
+		return np.arange(samplingRate * duration)
+
+	def createSineYCoordinates(self, samplingRate, samples, frequency, amplitude=1.0):
+		sinwave = 2 * np.pi * samples * frequency / samplingRate
+		wave = (amplitude * np.sin(sinwave)).astype(np.float32)
+
+		return wave
